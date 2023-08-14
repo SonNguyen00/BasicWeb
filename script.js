00176d8807c86da12b2291076ae96a7f35a6ab2c
SonNguyen00 <kawaken95@gmail.com>
2023-08-14 22:56:07 +0700

    a

---
+++ script.js
@@ -0,0 +1,22 @@
+// function : hàm
+// function Ten_Ham
+
+var name = prompt("Name");
+
+function function1(){
+    // Let : khai báo biến cục bộ
+    // Biến cục bộ thì sẽ
+    // không thể sử dụng ngoài function
+    // var tenbien : Khai báo biến toàn cục
+    name= document.getElementById('fullname').value;
+    console.log(name);
+}
+
+arr.push(name);
+const div = document.getElementsByClassName('div1');
+console.log(arr);
+var str=``;
+Array.forEach(el =>{
+    str+='<li>'+el+''
+});
+div[1]innerHTML="HelloWorld";
